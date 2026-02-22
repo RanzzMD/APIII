933a6acd4bb217ff6c504aec74fe11d3d26b37d6
RamzzzMD <ramdangani7905@gmail.com>
2026-02-22 23:52:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const fileUpload = require("express-fileupload"); // Tambahkan ini
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
+app.use(fileUpload());
 
 /*
 For setting API name etc
